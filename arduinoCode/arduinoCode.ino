0768d831dee47da1f25fcb06de272a13c45767bf
Cedric Honnet <honnet@telecom-paristech.org>
2019-08-01 18:31:22 +0200

    Refactoring.

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,63 +1,51 @@
+#ifdef CORE_TEENSY
+const int ADCbitDepth = 12;
+#else
+const int ADCbitDepth = 10; // typical arduino MCUs use 10bits ADC
+#endif
 
-//switching to teensy for better ADC
+// To calculate the resistance on the teensy, uncomment the following:
+//#define EMBED_RESISTANCE_CALCULATION
 
-int analogInPin = A5;  // Input from polymerized sensor
-int numberOfresister2 = 7;
-int resistor2Pin[] = {2, 3, 4, 5, 8, 9, 10};
-long resistor2Values[] = {10000000, 1000000, 100000, 10000, 1000, 220, 10};
-float Vin = 3.3;
+const int analogInPin = A5;  // Input from polymerized sensor
+const int numberOfResistors = 7;
+const int resistor2Pin[] = {2, 3, 4, 5, 8, 9, 10};
+const long resistor2Values[] = {10000000, 1000000, 100000, 10000, 1000, 220, 10};
+const float Vin = 3.3;
 
-//resistor 1 is the polymerized textile we want to measure
-//resistor 2 are switcheable resistors in the following range:
+int adcReadings[numberOfResistors] = {0}; //array for storing the readings
 
-//calculate resistor 1 using the formula Vout = Vin (R2/R1+r2)
-
-int adcReadings[] = {0, 0, 0, 0, 0, 0, 0}; //array for storing the readings
-
-/*
-  //if we want to calculate the resistance on the teensy
-  float voltOut[] = {0, 0, 0, 0, 0, 0, 0}; //array for storing the voltages (won't be used)
-  float resistanceEstimates[] = {0, 0, 0, 0, 0, 0, 0}; //array for storing the resistances (won't be used)
-*/
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-  analogReadResolution(12);
+  analogReadResolution(ADCbitDepth);
   pinMode(analogInPin, INPUT);
-  for (int i = 0; i < numberOfresister2; i++) { //set all pins as input, so that no current can flow
+  for (int i = 0; i < numberOfResistors; i++) { //set all pins as input, so that no current can flow
     pinMode(resistor2Pin[i], INPUT);
   }
 }
 
+
 void loop() {
 
 
-  for (int i = 0; i < numberOfresister2; i++) { //pull pins high one at a time and read, then pull low again.
-    pinMode(resistor2Pin[i], OUTPUT);
-    delay(2);
-    adcReadings[i] = analogRead(analogInPin); //multiple reads for making sure the value is as stable as can be
-    adcReadings[i] = analogRead(analogInPin);
-    adcReadings[i] = analogRead(analogInPin) + adcReadings[i]; //some averaging, to decrease noise
-    adcReadings[i] = analogRead(analogInPin) + adcReadings[i];
-    adcReadings[i] = analogRead(analogInPin) + adcReadings[i];
-    adcReadings[i] = analogRead(analogInPin) + adcReadings[i];
-    adcReadings[i] = analogRead(analogInPin) + adcReadings[i];
-    adcReadings[i] = adcReadings[i] / 6;
-    pinMode(resistor2Pin[i], INPUT);
-  }
-
-  /*
-     if we want to calculate the resistance on the teensy
+#ifdef EMBED_RESISTANCE_CALCULATION
+  //if we want to calculate the resistance on the teensy
+  float voltOut[numberOfResistors] = {0}; //array for storing the voltages (won't be used)
+  float resistanceEstimates[numberOfResistors] = {0}; //array for storing the resistances (won't be used)
 
     //find the voltage
-     for (int i = 0; i < numberOfresister2; i++) { //pull pins high one at a time and read, then pull low again.
-      voltOut[i] = map(adcReadings[i],0,1023,0,5000)/1000.0; //map to millivolt
+     for (int i = 0; i < numberOfResistors; i++) { //pull pins high one at a time and read, then pull low again.
+      voltOut[i] = map(adcReadings[i] , 0,1<<ADCbitDepth , 0,5000)/1000.0; //map to millivolt
     }
 
     //calculate the resistance
-    for (int i = 0; i < numberOfresister2; i++) { //pull pins high one at a time and read, then pull low again.
+    for (int i = 0; i < numberOfResistors; i++) { //pull pins high one at a time and read, then pull low again.
 
+     //calculate resistor 1 using the formula Vout = Vin (R2/R1+r2)
+     //resistor 1 is the polymerized textile we want to measure
+     //resistor 2 are switchable resistors in the following range:
      // R1 = ((Vi * R2) - (Vout * R2)) / Vout
 
      // calculating first two parts
@@ -66,15 +54,36 @@ void loop() {
 
       resistanceEstimates[i] = (VinXr2- VoutXr2)/ voltOut[i]; //again adjusting for millivolt
     }
-  */
 
   //print to serial
-  for (int i = 0; i < numberOfresister2 - 1; i++) { //set all pins as input, so that no current can flow
+  for (int i = 0; i < numberOfResistors - 1; i++) { //set all pins as input, so that no current can flow
+    Serial.print(resistanceEstimates[i]);
+    Serial.print(", ");
+  }
+  Serial.println(resistanceEstimates[numberOfResistors - 1]);
+  //end print to serial
+
+#else
+
+  for (int i = 0; i < numberOfResistors; i++) { //pull pins high one at a time and read, then pull low again.
+    pinMode(resistor2Pin[i], OUTPUT);
+    int averageNum = 0;
+    adcReadings[i] = 0;
+    for (int a = 0; a < averageNum; a++)
+        adcReadings[i] += analogRead(analogInPin); //some averaging, to decrease noise
+    adcReadings[i] = adcReadings[i] / averageNum;
+    pinMode(resistor2Pin[i], INPUT);
+  }
+
+  //print to serial
+  for (int i = 0; i < numberOfResistors - 1; i++) { //set all pins as input, so that no current can flow
     Serial.print(adcReadings[i]);
     Serial.print(", ");
   }
-  Serial.println(adcReadings[numberOfresister2 - 1]);
+  Serial.println(adcReadings[numberOfResistors - 1]);
   //end print to serial
+
+#endif
 
   delay(10);
 }
